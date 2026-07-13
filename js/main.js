fe511c90bef8f72419004e086a815bcea8d97355
cynthiachou <yc.n3x9@gmail.com>
2026-07-13 15:51:44 +0800

    07090119

--- js/main.js
+++ js/main.js
@@ -2,33 +2,43 @@ const book = document.getElementById("book");
 
 let isOpen = false;
 
+function openBook(){
+
+    book.classList.add("open");
+
+    setTimeout(() => {
+
+        book.classList.add("opened");
+
+    },400);
+
+    isOpen = true;
+
+}
+
+function closeBook(){
+
+    book.classList.remove("opened");
+
+    setTimeout(() => {
+
+        book.classList.remove("open");
+
+    },400);
+
+    isOpen = false;
+
+}
+
 book.addEventListener("click", () => {
 
-    if(!isOpen){
+    if(isOpen){
 
-        book.classList.add("open");
+        closeBook();
 
-        setTimeout(() => {
+    }else{
 
-            book.classList.add("opened");
-
-        },400);
-
-        isOpen = true;
-
-    }
-
-    else{
-
-        book.classList.remove("opened");
-
-        setTimeout(() => {
-
-            book.classList.remove("open");
-
-        },400);
-
-        isOpen = false;
+        openBook();
 
     }
 
